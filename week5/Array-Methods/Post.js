24107cfb637aa8cb6a5007a61ce7e282d66438ae
sewoolson <samwoolson@gmail.com>
2023-05-07 16:53:24 -0500

    corrected

--- week5/Array-Methods/Post.js
+++ week5/Array-Methods/Post.js
@@ -1,5 +1,7 @@
-const data =
-[
+const { title } = require("process");
+
+const data = [
+
   {
     "userId": 1,
     "id": 1,
@@ -302,8 +304,360 @@ const data =
   }
 ]
 
+
 // create a list of user 5's posts
-// data.forEach((element,[5]) => console.log(element,[5]));
-//overall I'm not sure on how to call userId: 5 from index 40-50.
+let user5Posts = data.filter(info => info.userId === 5);
+console.log(user5Posts);
+
+//returns
+// [
+//   {
+//     userId: 5,
+//     id: 41,
+//     title: 'non est facere',
+//     body: 'molestias id nostrum\n' +
+//       'excepturi molestiae dolore omnis repellendus quaerat saepe\n' +
+//       'consectetur iste quaerat tenetur asperiores accusamus ex ut\n' +
+//       'nam quidem est ducimus sunt debitis saepe'
+//   },
+//   {
+//     userId: 5,
+//     id: 42,
+//     title: 'commodi ullam sint et excepturi error explicabo praesentium voluptas',
+//     body: 'odio fugit voluptatum ducimus earum autem est incidunt voluptatem\n' +
+//       'odit reiciendis aliquam sunt sequi nulla dolorem\n' +
+//       'non facere repellendus voluptates quia\n' +
+//       'ratione harum vitae ut'
+//   },
+//   {
+//     userId: 5,
+//     id: 43,
+//     title: 'eligendi iste nostrum consequuntur adipisci praesentium sit beatae perferendis',
+//     body: 'similique fugit est\n' +
+//       'illum et dolorum harum et voluptate eaque quidem\n' +
+//       'exercitationem quos nam commodi possimus cum odio nihil nulla\n' +
+//       'dolorum exercitationem magnam ex et a et distinctio debitis'
+//   },
+//   {
+//     userId: 5,
+//     id: 44,
+//     title: 'optio dolor molestias sit',
+//     body: 'temporibus est consectetur dolore\n' +
+//       'et libero debitis vel velit laboriosam quia\n' +
+//       'ipsum quibusdam qui itaque fuga rem aut\n' +
+//       'ea et iure quam sed maxime ut distinctio quae'
+//   },
+//   {
+//     userId: 5,
+//     id: 45,
+//     title: 'ut numquam possimus omnis eius suscipit laudantium iure',
+//     body: 'est natus reiciendis nihil possimus aut provident\n' +
+//       'ex et dolor\n' +
+//       'repellat pariatur est\n' +
+//       'nobis rerum repellendus dolorem autem'
+//   },
+//   {
+//     userId: 5,
+//     id: 46,
+//     title: 'aut quo modi neque nostrum ducimus',
+//     body: 'voluptatem quisquam iste\n' +
+//       'voluptatibus natus officiis facilis dolorem\n' +
+//       'quis quas ipsam\n' +
+//       'vel et voluptatum in aliquid'
+//   },
+//   {
+//     userId: 5,
+//     id: 47,
+//     title: 'quibusdam cumque rem aut deserunt',
+//     body: 'voluptatem assumenda ut qui ut cupiditate aut impedit veniam\n' +
+//       'occaecati nemo illum voluptatem laudantium\n' +
+//       'molestiae beatae rerum ea iure soluta nostrum\n' +
+//       'eligendi et voluptate'
+//   },
+//   {
+//     userId: 5,
+//     id: 48,
+//     title: 'ut voluptatem illum ea doloribus itaque eos',
+//     body: 'voluptates quo voluptatem facilis iure occaecati\n' +
+//       'vel assumenda rerum officia et\n' +
+//       'illum perspiciatis ab deleniti\n' +
+//       'laudantium repellat ad ut et autem reprehenderit'
+//   },
+//   {
+//     userId: 5,
+//     id: 49,
+//     title: 'laborum non sunt aut ut assumenda perspiciatis voluptas',
+//     body: 'inventore ab sint\n' +
+//       'natus fugit id nulla sequi architecto nihil quaerat\n' +
+//       'eos tenetur in in eum veritatis non\n' +
+//       'quibusdam officiis aspernatur cumque aut commodi aut'
+//   },
+//   {
+//     userId: 5,
+//     id: 50,
+//     title: 'repellendus qui recusandae incidunt voluptates tenetur qui omnis exercitationem',
+//     body: 'error suscipit maxime adipisci consequuntur recusandae\n' +
+//       'voluptas eligendi et est et voluptates\n' +
+//       'quia distinctio ab amet quaerat molestiae et vitae\n' +
+//       'adipisci impedit sequi nesciunt quis consectetur'
+//   }
+// ]
 
 // then, create a list of title and body pairs
+let array = [];
+data.forEach((arr) => array.push(arr.title) &&array.push(arr.body));
+console.log(array);
+
+//returns
+// [
+//   'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+//   'quia et suscipit\n' +
+//     'suscipit recusandae consequuntur expedita et cum\n' +
+//     'reprehenderit molestiae ut ut quas totam\n' +
+//     'nostrum rerum est autem sunt rem eveniet architecto',
+//   'qui est esse',
+//   'est rerum tempore vitae\n' +
+//     'sequi sint nihil reprehenderit dolor beatae ea dolores neque\n' +
+//     'fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\n' +
+//     'qui aperiam non debitis possimus qui neque nisi nulla',
+//   'ea molestias quasi exercitationem repellat qui ipsa sit aut',
+//   'et iusto sed quo iure\n' +
+//     'voluptatem occaecati omnis eligendi aut ad\n' +
+//     'voluptatem doloribus vel accusantium quis pariatur\n' +
+//     'molestiae porro eius odio et labore et velit aut',
+//   'eum et est occaecati',
+//   'ullam et saepe reiciendis voluptatem adipisci\n' +
+//     'sit amet autem assumenda provident rerum culpa\n' +
+//     'quis hic commodi nesciunt rem tenetur doloremque ipsam iure\n' +
+//     'quis sunt voluptatem rerum illo velit',
+//   'nesciunt quas odio',
+//   'repudiandae veniam quaerat sunt sed\n' +
+//     'alias aut fugiat sit autem sed est\n' +
+//     'voluptatem omnis possimus esse voluptatibus quis\n' +
+//     'est aut tenetur dolor neque',
+//   'dolorem eum magni eos aperiam quia',
+//   'ut aspernatur corporis harum nihil quis provident sequi\n' +
+//     'mollitia nobis aliquid molestiae\n' +
+//     'perspiciatis et ea nemo ab reprehenderit accusantium quas\n' +
+//     'voluptate dolores velit et doloremque molestiae',
+//   'magnam facilis autem',
+//   'dolore placeat quibusdam ea quo vitae\n' +
+//     'magni quis enim qui quis quo nemo aut saepe\n' +
+//     'quidem repellat excepturi ut quia\n' +
+//     'sunt ut sequi eos ea sed quas',
+//   'dolorem dolore est ipsam',
+//   'dignissimos aperiam dolorem qui eum\n' +
+//     'facilis quibusdam animi sint suscipit qui sint possimus cum\n' +
+//     'quaerat magni maiores excepturi\n' +
+//     'ipsam ut commodi dolor voluptatum modi aut vitae',
+//   'nesciunt iure omnis dolorem tempora et accusantium',
+//   'consectetur animi nesciunt iure dolore\n' +
+//     'enim quia ad\n' +
+//     'veniam autem ut quam aut nobis\n' +
+//     'et est aut quod aut provident voluptas autem voluptas',
+//   'optio molestias id quia eum',
+//   'quo et expedita modi cum officia vel magni\n' +
+//     'doloribus qui repudiandae\n' +
+//     'vero nisi sit\n' +
+//     'quos veniam quod sed accusamus veritatis error',
+//   'et ea vero quia laudantium autem',
+//   'delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\n' +
+//     'accusamus in eum beatae sit\n' +
+//     'vel qui neque voluptates ut commodi qui incidunt\n' +
+//     'ut animi commodi',
+//   'in quibusdam tempore odit est dolorem',
+//   'itaque id aut magnam\n' +
+//     'praesentium quia et ea odit et ea voluptas et\n' +
+//     'sapiente quia nihil amet occaecati quia id voluptatem\n' +
+//     'incidunt ea est distinctio odio',
+//   'dolorum ut in voluptas mollitia et saepe quo animi',
+//   'aut dicta possimus sint mollitia voluptas commodi quo doloremque\n' +
+//     'iste corrupti reiciendis voluptatem eius rerum\n' +
+//     'sit cumque quod eligendi laborum minima\n' +
+//     'perferendis recusandae assumenda consectetur porro architecto ipsum ipsam',
+//   'voluptatem eligendi optio',
+//   'fuga et accusamus dolorum perferendis illo voluptas\n' +
+//     'non doloremque neque facere\n' +
+//     'ad qui dolorum molestiae beatae\n' +
+//     'sed aut voluptas totam sit illum',
+//   'eveniet quod temporibus',
+//   'reprehenderit quos placeat\n' +
+//     'velit minima officia dolores impedit repudiandae molestiae nam\n' +
+//     'voluptas recusandae quis delectus\n' +
+//     'officiis harum fugiat vitae',
+//   'sint suscipit perspiciatis velit dolorum rerum ipsa laboriosam odio',
+//   'suscipit nam nisi quo aperiam aut\n' +
+//     'asperiores eos fugit maiores voluptatibus quia\n' +
+//     'voluptatem quis ullam qui in alias quia est\n' +
+//     'consequatur magni mollitia accusamus ea nisi voluptate dicta',
+//   'fugit voluptas sed molestias voluptatem provident',
+//   'eos voluptas et aut odit natus earum\n' +
+//     'aspernatur fuga molestiae ullam\n' +
+//     'deserunt ratione qui eos\n' +
+//     'qui nihil ratione nemo velit ut aut id quo',
+//   'voluptate et itaque vero tempora molestiae',
+//   'eveniet quo quis\n' +
+//     'laborum totam consequatur non dolor\n' +
+//     'ut et est repudiandae\n' +
+//     'est voluptatem vel debitis et magnam',
+//   'adipisci placeat illum aut reiciendis qui',
+//   'illum quis cupiditate provident sit magnam\n' +
+//     'ea sed aut omnis\n' +
+//     'veniam maiores ullam consequatur atque\n' +
+//     'adipisci quo iste expedita sit quos voluptas',
+//   'doloribus ad provident suscipit at',
+//   'qui consequuntur ducimus possimus quisquam amet similique\n' +
+//     'suscipit porro ipsam amet\n' +
+//     'eos veritatis officiis exercitationem vel fugit aut necessitatibus totam\n' +
+//     'omnis rerum consequatur expedita quidem cumque explicabo',
+//   'asperiores ea ipsam voluptatibus modi minima quia sint',
+//   'repellat aliquid praesentium dolorem quo\n' +
+//     'sed totam minus non itaque\n' +
+//     'nihil labore molestiae sunt dolor eveniet hic recusandae veniam\n' +
+//     'tempora et tenetur expedita sunt',
+//   'dolor sint quo a velit explicabo quia nam',
+//   'eos qui et ipsum ipsam suscipit aut\n' +
+//     'sed omnis non odio\n' +
+//     'expedita earum mollitia molestiae aut atque rem suscipit\n' +
+//     'nam impedit esse',
+//   'maxime id vitae nihil numquam',
+//   'veritatis unde neque eligendi\n' +
+//     'quae quod architecto quo neque vitae\n' +
+//     'est illo sit tempora doloremque fugit quod\n' +
+//     'et et vel beatae sequi ullam sed tenetur perspiciatis',
+//   'autem hic labore sunt dolores incidunt',
+//   'enim et ex nulla\n' +
+//     'omnis voluptas quia qui\n' +
+//     'voluptatem consequatur numquam aliquam sunt\n' +
+//     'totam recusandae id dignissimos aut sed asperiores deserunt',
+//   'rem alias distinctio quo quis',
+//   'ullam consequatur ut\n' +
+//     'omnis quis sit vel consequuntur\n' +
+//     'ipsa eligendi ipsum molestiae et omnis error nostrum\n' +
+//     'molestiae illo tempore quia et distinctio',
+//   'est et quae odit qui non',
+//   'similique esse doloribus nihil accusamus\n' +
+//     'omnis dolorem fuga consequuntur reprehenderit fugit recusandae temporibus\n' +
+//     'perspiciatis cum ut laudantium\n' +
+//     'omnis aut molestiae vel vero',
+//   'quasi id et eos tenetur aut quo autem',
+//   'eum sed dolores ipsam sint possimus debitis occaecati\n' +
+//     'debitis qui qui et\n' +
+//     'ut placeat enim earum aut odit facilis\n' +
+//     'consequatur suscipit necessitatibus rerum sed inventore temporibus consequatur',
+//   'delectus ullam et corporis nulla voluptas sequi',
+//   'non et quaerat ex quae ad maiores\n' +
+//     'maiores recusandae totam aut blanditiis mollitia quas illo\n' +
+//     'ut voluptatibus voluptatem\n' +
+//     'similique nostrum eum',
+//   'iusto eius quod necessitatibus culpa ea',
+//   'odit magnam ut saepe sed non qui\n' +
+//     'tempora atque nihil\n' +
+//     'accusamus illum doloribus illo dolor\n' +
+//     'eligendi repudiandae odit magni similique sed cum maiores',
+//   'a quo magni similique perferendis',
+//   'alias dolor cumque\n' +
+//     'impedit blanditiis non eveniet odio maxime\n' +
+//     'blanditiis amet eius quis tempora quia autem rem\n' +
+//     'a provident perspiciatis quia',
+//   'ullam ut quidem id aut vel consequuntur',
+//   'debitis eius sed quibusdam non quis consectetur vitae\n' +
+//     'impedit ut qui consequatur sed aut in\n' +
+//     'quidem sit nostrum et maiores adipisci atque\n' +
+//     'quaerat voluptatem adipisci repudiandae',
+//   'doloremque illum aliquid sunt',
+//   'deserunt eos nobis asperiores et hic\n' +
+//     'est debitis repellat molestiae optio\n' +
+//     'nihil ratione ut eos beatae quibusdam distinctio maiores\n' +
+//     'earum voluptates et aut adipisci ea maiores voluptas maxime',
+//   'qui explicabo molestiae dolorem',
+//   'rerum ut et numquam laborum odit est sit\n' +
+//     'id qui sint in\n' +
+//     'quasi tenetur tempore aperiam et quaerat qui in\n' +
+//     'rerum officiis sequi cumque quod',
+//   'magnam ut rerum iure',
+//   'ea velit perferendis earum ut voluptatem voluptate itaque iusto\n' +
+//     'totam pariatur in\n' +
+//     'nemo voluptatem voluptatem autem magni tempora minima in\n' +
+//     'est distinctio qui assumenda accusamus dignissimos officia nesciunt nobis',
+//   'id nihil consequatur molestias animi provident',
+//   'nisi error delectus possimus ut eligendi vitae\n' +
+//     'placeat eos harum cupiditate facilis reprehenderit voluptatem beatae\n' +
+//     'modi ducimus quo illum voluptas eligendi\n' +
+//     'et nobis quia fugit',
+//   'fuga nam accusamus voluptas reiciendis itaque',
+//   'ad mollitia et omnis minus architecto odit\n' +
+//     'voluptas doloremque maxime aut non ipsa qui alias veniam\n' +
+//     'blanditiis culpa aut quia nihil cumque facere et occaecati\n' +
+//     'qui aspernatur quia eaque ut aperiam inventore',
+//   'provident vel ut sit ratione est',
+//   'debitis et eaque non officia sed nesciunt pariatur vel\n' +
+//     'voluptatem iste vero et ea\n' +
+//     'numquam aut expedita ipsum nulla in\n' +
+//     'voluptates omnis consequatur aut enim officiis in quam qui',
+//   'explicabo et eos deleniti nostrum ab id repellendus',
+//   'animi esse sit aut sit nesciunt assumenda eum voluptas\n' +
+//     'quia voluptatibus provident quia necessitatibus ea\n' +
+//     'rerum repudiandae quia voluptatem delectus fugit aut id quia\n' +
+//     'ratione optio eos iusto veniam iure',
+//   'eos dolorem iste accusantium est eaque quam',
+//   'corporis rerum ducimus vel eum accusantium\n' +
+//     'maxime aspernatur a porro possimus iste omnis\n' +
+//     'est in deleniti asperiores fuga aut\n' +
+//     'voluptas sapiente vel dolore minus voluptatem incidunt ex',
+//   'enim quo cumque',
+//   'ut voluptatum aliquid illo tenetur nemo sequi quo facilis\n' +
+//     'ipsum rem optio mollitia quas\n' +
+//     'voluptatem eum voluptas qui\n' +
+//     'unde omnis voluptatem iure quasi maxime voluptas nam',
+//   'non est facere',
+//   'molestias id nostrum\n' +
+//     'excepturi molestiae dolore omnis repellendus quaerat saepe\n' +
+//     'consectetur iste quaerat tenetur asperiores accusamus ex ut\n' +
+//     'nam quidem est ducimus sunt debitis saepe',
+//   'commodi ullam sint et excepturi error explicabo praesentium voluptas',
+//   'odio fugit voluptatum ducimus earum autem est incidunt voluptatem\n' +
+//     'odit reiciendis aliquam sunt sequi nulla dolorem\n' +
+//     'non facere repellendus voluptates quia\n' +
+//     'ratione harum vitae ut',
+//   'eligendi iste nostrum consequuntur adipisci praesentium sit beatae perferendis',
+//   'similique fugit est\n' +
+//     'illum et dolorum harum et voluptate eaque quidem\n' +
+//     'exercitationem quos nam commodi possimus cum odio nihil nulla\n' +
+//     'dolorum exercitationem magnam ex et a et distinctio debitis',
+//   'optio dolor molestias sit',
+//   'temporibus est consectetur dolore\n' +
+//     'et libero debitis vel velit laboriosam quia\n' +
+//     'ipsum quibusdam qui itaque fuga rem aut\n' +
+//     'ea et iure quam sed maxime ut distinctio quae',
+//   'ut numquam possimus omnis eius suscipit laudantium iure',
+//   'est natus reiciendis nihil possimus aut provident\n' +
+//     'ex et dolor\n' +
+//     'repellat pariatur est\n' +
+//     'nobis rerum repellendus dolorem autem',
+//   'aut quo modi neque nostrum ducimus',
+//   'voluptatem quisquam iste\n' +
+//     'voluptatibus natus officiis facilis dolorem\n' +
+//     'quis quas ipsam\n' +
+//     'vel et voluptatum in aliquid',
+//   'quibusdam cumque rem aut deserunt',
+//   'voluptatem assumenda ut qui ut cupiditate aut impedit veniam\n' +
+//     'occaecati nemo illum voluptatem laudantium\n' +
+//     'molestiae beatae rerum ea iure soluta nostrum\n' +
+//     'eligendi et voluptate',
+//   'ut voluptatem illum ea doloribus itaque eos',
+//   'voluptates quo voluptatem facilis iure occaecati\n' +
+//     'vel assumenda rerum officia et\n' +
+//     'illum perspiciatis ab deleniti\n' +
+//     'laudantium repellat ad ut et autem reprehenderit',
+//   'laborum non sunt aut ut assumenda perspiciatis voluptas',
+//   'inventore ab sint\n' +
+//     'natus fugit id nulla sequi architecto nihil quaerat\n' +
+//     'eos tenetur in in eum veritatis non\n' +
+//     'quibusdam officiis aspernatur cumque aut commodi aut',
+//   'repellendus qui recusandae incidunt voluptates tenetur qui omnis exercitationem',
+//   'error suscipit maxime adipisci consequuntur recusandae\n' +
+//     'voluptas eligendi et est et voluptates\n' +
+//     'quia distinctio ab amet quaerat molestiae et vitae\n' +
+//     'adipisci impedit sequi nesciunt quis consectetur'
+// ]
